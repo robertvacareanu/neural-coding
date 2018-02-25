bff93c45625a03a5ca832949323b6b9f5a295aba
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-02-25 14:00:06 +0200

    Added CSV exporter

---
+++ src/main/kotlin/logger/Exporter.kt
@@ -0,0 +1,19 @@
+package logger
+
+import java.io.File
+
+/**
+ * Created by robert on 2/25/18.
+ * Export the data in a csv manner. The results are put into a separate file
+ */
+fun exportCSV(data: List<Pair<Int, FloatArray>>, name: String) {
+    File(name).bufferedWriter().use { br ->
+        data.forEach {
+            br.appendln(it.second.joinToString(","))
+        }
+    }
+    File("${name}_Results").bufferedWriter().use { br ->
+        br.append(data.map { it.first }.joinToString())
+
+    }
+}
