87971253cb7b4cc449e284f45563a170ba882c32
Andrei Pop <Andrei.Pop@catalysts.cc>
2018-03-10 16:21:03 +0200

    Updated python script for generating plots

--- python_scripts/lda_plots.py
+++ python_scripts/lda_plots.py
@@ -1,22 +1,39 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
-from numpy import genfromtxt
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import sys
 
-data = genfromtxt('data', delimiter=',')
-X = data
-X_norm = (X-X.min())/(X.max()-X.min())
-y = genfromtxt('results', delimiter=',')
 
-#cols = ['c2', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13', 'c14', 'c15', 'c16', 'c17', 'c18', 'c19', 'c20', 'c21', 'c22', 'c23', 'c24', 'c25', 'c26', 'c27', 'c28', 'c29', 'c30', 'c31', 'c32']
+def apply_lda(data_file):
+    banknotes_data = pd.read_csv(data_file)
+    x = banknotes_data.iloc[:, :-1].values
+    y = banknotes_data.iloc[:, -1].values
 
-lda = LDA(n_components=2)
-lda_transformed = pd.DataFrame(lda.fit_transform(X_norm, y))
+    x_norm = (x - x.min()) / (x.max() - x.min())
 
-plt.scatter(lda_transformed[y==0][0], lda_transformed[y==0][1], label='0', c='red')
-plt.scatter(lda_transformed[y==1][0], lda_transformed[y==1][1], label='45', c='blue')
+    lda = LDA(n_components=2)
+    lda_transformed = pd.DataFrame(lda.fit_transform(x_norm, y))
 
-plt.legend()
-plt.show()
+    return y, lda_transformed
 
+
+def plot_data(lda_transformed, y):
+    plt.scatter(lda_transformed[y == 0][0], lda_transformed[y == 0][1], label='0', c='red')
+    plt.scatter(lda_transformed[y == 45][0], lda_transformed[y == 45][1], label='45', c='blue')
+
+    plt.title("LDA Reduction")
+    plt.legend()
+    plt.show()
+
+
+def main(argv):
+
+    if len(argv) == 0:
+        print "No input file provided as parameter!"
+    else:
+        y, lda_transformed = apply_lda(argv[0])
+        plot_data(lda_transformed, y)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
