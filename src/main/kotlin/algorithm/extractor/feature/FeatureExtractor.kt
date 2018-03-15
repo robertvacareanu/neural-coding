4d11f2d0bd2091bcd74f5eb18451a51438b8a961
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-03-15 16:11:51 +0200

    Remove units with empty data
    Added functionality for removing the units that don't have data for all trials

--- src/main/kotlin/algorithm/extractor/feature/FeatureExtractor.kt
+++ src/main/kotlin/algorithm/extractor/feature/FeatureExtractor.kt
@@ -2,6 +2,7 @@ package algorithm.extractor.feature
 
 /**
  * Created by robert on 2/15/18.
+ * Every implementation is expected to maintain the order
  */
 interface FeatureExtractor<in Data, out Features> {
     fun extract(data: Collection<Data>): Features
---
+++ src/main/kotlin/algorithm/processor/FeatureSelector.kt
@@ -0,0 +1,22 @@
+package algorithm.processor
+
+import reader.almostEqual
+import reader.get
+
+/**
+ * Created by robert on 3/15/18.
+ * An utility function to remove from the data those units that don't contain data in all trials
+ */
+fun removeIfEmpty(data: List<Pair<Int, FloatArray>>) = with(data) {
+    val emptyColumns = mutableSetOf<Int>()
+    forEach { unitData ->
+        emptyColumns.addAll(unitData.second.indices.filter { unitData[it] almostEqual 0.0 })
+    }
+
+    val result = mutableListOf<Pair<Int, FloatArray>>()
+
+    forEach {
+        result.add(Pair(it.first, it.second.filterIndexed { index, _ -> !emptyColumns.contains(index) }.toFloatArray()))
+    }
+    result
+}
