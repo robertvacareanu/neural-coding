4a32a6e448b6c4e258f8be23d98e8a2ce3b9988d
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-03-18 16:11:56 +0200

    SVM plot script improvements

--- python_scripts/svm.py
+++ python_scripts/svm.py
@@ -11,7 +11,7 @@ style.use("ggplot")
 from sklearn import svm
 
 
-def plot_svm(path, orientation1=0, orientation2=45, save_path=None):
+def plot_svm(path, orientation1, orientation2, color1, color2, save_path):
     data = pd.read_csv(path)
     X = data.iloc[:, :-1].values
     Y = data.iloc[:, -1].values
@@ -25,9 +25,9 @@ def plot_svm(path, orientation1=0, orientation2=45, save_path=None):
     new_y = Y[np.logical_or(Y == orientation1, Y == orientation2)]
 
     plt.scatter(lda_transformed[Y == orientation1][0], lda_transformed[Y == orientation1][1], label=orientation1,
-                c='red')
+                c=color1)
     plt.scatter(lda_transformed[Y == orientation2][0], lda_transformed[Y == orientation2][1], label=orientation2,
-                c='blue')
+                c=color2)
 
     clf = svm.NuSVC(kernel='rbf')
     clf.fit(new_x, new_y)
@@ -56,9 +56,12 @@ def plot_svm(path, orientation1=0, orientation2=45, save_path=None):
 
 parser = argparse.ArgumentParser(description="Script to plot svm after applying lda to data")
 parser.add_argument("-p", "--path", required=True, help="Path to the file containing the file with the features")
-parser.add_argument("-o", "--orientation", nargs="*", required=False, default=[0, 45],
+parser.add_argument("-o", "--orientation", nargs="*", type=int, required=False, default=[0, 45],
                     help="Orientation to plot svm for. Default is 0 and 45")
-parser.add_argument("-s", "--save", required=False, default=None, help="Path to save the plot. By default only displays the plot")
+parser.add_argument("-c", "--color", nargs="*", required=False, default=['red', 'blue'],
+                    help="Colors to plot the points corresponding to the two orientations")
+parser.add_argument("-s", "--save", required=False, default=None,
+                    help="Path to save the plot. By default only displays the plot")
 
 result = parser.parse_args(sys.argv[1:])
-plot_svm(result.path, result.orientation[0], result.orientation[1], result.save)
+plot_svm(result.path, result.orientation[0], result.orientation[1], result.color[0], result.color[1], result.save)
