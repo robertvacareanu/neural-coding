12c78e582e82e2bb0542422e4e6a5bccd6daa14c
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-04-24 20:16:00 +0300

    Small modifications. Spike count value extractor

--- src/main/kotlin/algorithm/extractor/value/TimeToFirstSpike.kt
+++ src/main/kotlin/algorithm/extractor/value/TimeToFirstSpike.kt
@@ -7,11 +7,6 @@ import model.Spike
  *
  */
 class TimeToFirstSpike(private val waveformInternalSampling: Float, private val eventStartTimes: List<Int>) : ValueExtractor<Spike, Float> {
-    //    override fun extractValue(values: Array<Spike>): Float {
-//        values.forEach { println(it.timestamp) }
-//        val s = values.minBy { it.timestamp }!!
-//        return ((s.timestamp.toInt() - eventStartTimes.first { s.timestamp > it }).toDouble()/waveformInternalSampling.toDouble()).toFloat()
-//    }
     override fun extractValue(values: Array<Spike>) = with(values.minBy { it.timestamp }) {
         if (this != null) {
             ((timestamp.toInt() - eventStartTimes.last { timestamp > it }).toDouble() / waveformInternalSampling.toDouble()).toFloat()
