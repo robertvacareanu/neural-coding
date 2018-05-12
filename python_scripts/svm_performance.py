edcfba46f60c5323fa186bc50f31339d2fbe8293
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-05-12 22:07:27 +0300

    SVM script improvements

--- python_scripts/svm_performance.py
+++ python_scripts/svm_performance.py
@@ -4,40 +4,45 @@ import numpy as np
 import pandas as pd
 from matplotlib import style
 from sklearn import metrics
-from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 style.use("ggplot")
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import PCA
 
-def svm_comparison(path, orientation1, orientation2, histogram, lda):
+
+def svm_comparison(path, orientations, histogram, repetitions, size, pca_percent):
     data = pd.read_csv(path, header=None)
     X = data.iloc[:, :-1].values
     Y = data.iloc[:, -1].values
     scaler = StandardScaler()
     X_norm = scaler.fit_transform(X)
 
-    new_x = X_norm[np.logical_or(Y == orientation1, Y == orientation2)]
-    new_y = Y[np.logical_or(Y == orientation1, Y == orientation2)]
+    if orientations is not None:
+        new_x = X_norm[np.logical_or(Y == orientations[0], Y == orientations[1])]
+        new_y = Y[np.logical_or(Y == orientations[0], Y == orientations[1])]
+    else:
+        new_x = X_norm
+        new_y = Y
 
-    if lda:
-        lda = LDA()
-        lda_transformed = pd.DataFrame(lda.fit_transform(new_x, new_y))
-        new_x = np.append(new_x, lda_transformed, axis=1)
+    if pca_percent is not None:
+        pca = PCA(n_components=new_x.shape[1])
+        pca.fit(new_x)
+        var = np.cumsum(np.round(pca.explained_variance_ratio_, decimals=10) * 100)
+        transformed = pca.transform(new_x)
+        index = np.where(var <= pca_percent)[0][-1] + 1
+        new_x = transformed[:, :index]
 
     scores1 = []
-    scores2 = []
-    for _ in range(0, 10000):
-        X_train, X_test, y_train, y_test = train_test_split(new_x, new_y, test_size=0.25)
-        classifier = SVC(kernel='rbf', gamma=0.015625, C=32)
+    for _ in range(0, repetitions):
+        X_train, X_test, y_train, y_test = train_test_split(new_x, new_y, test_size=size)
+
+        classifier = SVC()
         classifier.fit(X_train, y_train)
         scores1.append(metrics.accuracy_score(y_test, classifier.predict(X_test)))
-        scores2.append(metrics.accuracy_score(new_y, classifier.predict(new_x)))
-    print(np.mean(scores1), np.mean(scores2))
-    print(np.std(scores1), np.std(scores2))
-    print(np.min(scores1), scores1.count(np.min(scores1)), np.max(scores1), scores1.count(np.max(scores1)))
-    print(np.min(scores2), np.max(scores2))
+    print(np.mean(scores1))
+    print(np.std(scores1))
 
     if histogram:
         plt.title(path)
@@ -47,10 +52,14 @@ def svm_comparison(path, orientation1, orientation2, histogram, lda):
 
 parser = argparse.ArgumentParser(description="Script to print the performa")
 parser.add_argument("-p", "--path", required=True, help="Path to the file with the features")
-parser.add_argument("-o", "--orientation", nargs="*", type=int, required=False, default=[0, 90],
+parser.add_argument("-o", "--orientation", nargs="*", type=int, required=False, default=None,
                     help="Orientation to plot svm for. Default is 0 and 90")
 parser.add_argument("--histogram", action='store_true',
                     help="Plot histogram with the results")
-parser.add_argument("-l", "--lda", required=False, action='store_true', help="Apply lda on data")
+parser.add_argument("-r", "--repetitions", type=int, required=False, default=1000)
+parser.add_argument("-s", "--size", type=float, required=False, default=0.2)
+parser.add_argument("--pca", type=float, required=False, default=None, help="Percentage of variance to take")
 result = parser.parse_args()
-svm_comparison(result.path, result.orientation[0], result.orientation[1], result.histogram, result.lda)
+svm_comparison(result.path, result.orientation, result.histogram,
+               result.repetitions,
+               result.size, result.pca)
