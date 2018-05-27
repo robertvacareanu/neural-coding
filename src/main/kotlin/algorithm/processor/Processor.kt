9b30e20f5095a9551729bd33d966e64512969afd
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-05-27 14:09:57 +0300

    Default processor for sorted and unsorted

--- src/main/kotlin/algorithm/processor/Processor.kt
+++ src/main/kotlin/algorithm/processor/Processor.kt
@@ -31,19 +31,40 @@ fun process(spikeReader: SpikeReader,
         postProcessingTransformers.fold(featureExtractor.extract(preProcessingTransformers.fold(dataExtractor.extractData(spikeReader.readTrials())) { acc, function -> function(acc) }, valueExtractor::extractValue)) { acc, function -> function(acc) }
 
 /**
- * An utility function to simplify the whole process
+ * An utility function to simplify the whole process of generating feature file using unsorted dataset
  * Contains defaults for the most basic approach: single value extractor, unsorted data set, between stim and mean amplitude
  * The transformers are applied in order, starting from left
  *  - preProcessing -> after extracting the data using @param dataExtractor
  *  - postProcessing -> after computing the features with @param valueExtractor using @param featureExtractor
  */
-fun process(path: String,
-            preProcessingTransformers: List<(List<TrialData>) -> List<TrialData>> = listOf(),
-            postProcessingTransformers: List<(DataSet) -> DataSet> = listOf()
+fun processUnsorted(path: String,
+                    preProcessingTransformers: List<(List<TrialData>) -> List<TrialData>> = listOf(),
+                    postProcessingTransformers: List<(DataSet) -> DataSet> = listOf()
 ): DataSet {
     val mr = MetadataReader(path)
     val spktwe = mr.readSPKTWE()
     val sp = DataSpikeReader(mr.readETI(), SpikeMetadata(spktwe))
+    val ve = MeanAmplitude(spktwe.waveformSpikeOffset)
+    val de = BetweenStim(sp)
+    val fe = SingleValueFeatureExtractor()
+
+    return postProcessingTransformers.fold(fe.extract(preProcessingTransformers.fold(de.extractData(sp.readTrials())) { acc, function -> function(acc) }, ve::extractValue)) { acc, function -> function(acc) }
+}
+
+/**
+ * An utility function to simplify the whole process of generating feature file using sorted dataset
+ * Contains defaults for the most basic approach: single value extractor, unsorted data set, between stim and mean amplitude
+ * The transformers are applied in order, starting from left
+ *  - preProcessing -> after extracting the data using @param dataExtractor
+ *  - postProcessing -> after computing the features with @param valueExtractor using @param featureExtractor
+ */
+fun processSorted(path: String,
+                  preProcessingTransformers: List<(List<TrialData>) -> List<TrialData>> = listOf(),
+                  postProcessingTransformers: List<(DataSet) -> DataSet> = listOf()
+): DataSet {
+    val mr = MetadataReader(path)
+    val spktwe = mr.readSPKTWE()
+    val sp = DataSpikeReader(mr.readETI(), SpikeMetadata(mr.readSSD()))
     val ve = MeanAmplitude(spktwe.waveformSpikeOffset)
     val de = BetweenStim(sp)
     val fe = SingleValueFeatureExtractor()
