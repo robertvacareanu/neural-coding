8af2d260c3157ce7540b90b7ba1f1b25deb536f8
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-05-30 00:06:53 +0300

    Importer

---
+++ src/main/kotlin/importer/Importer.kt
@@ -0,0 +1,20 @@
+package importer
+
+import main.DataPoint
+import main.DataSet
+import java.io.File
+
+/**
+ * Created by robert on 5/30/18.
+ * Read the file from the given path into a dataSet
+ */
+fun read(path: String): DataSet {
+    val file = File(path)
+    val result = mutableListOf<DataPoint>()
+    file.readLines().mapTo(result) {
+        val values = it.split(",")
+        DataPoint(values.last().toInt(), values.dropLast(1).map { it.toFloat() }.toFloatArray())
+    }
+
+    return result
+}
