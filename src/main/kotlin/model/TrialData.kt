392384bb1bd3c49b76fc21cfd9fc1f8def3d6d66
Robert Vacareanu <robert.vacareanu@gmail.com>
2018-06-03 15:07:34 +0300

    Multiple ways of retrieving data from a TrialData

--- src/main/kotlin/model/TrialData.kt
+++ src/main/kotlin/model/TrialData.kt
@@ -6,5 +6,26 @@ package model
  * spikeData represents the spikes recorded by a recording entity
  */
 data class TrialData(val orientation: Int, val spikeData: List<Array<Spike>>, val extractedBetween: Pair<Double, Double>) {
-    operator fun get(index: Int) = spikeData[index]
+
+    /**
+     * @param unit = the unit for which to return the spikes
+     * @return An array of spikes corresponding to [unit]
+     */
+    operator fun get(unit: Int) = spikeData[unit]
+
+    /**
+     * @param unit - which unit
+     * @param spike - which spike
+     * @return The spike corresponding to [unit] and [spike]
+     */
+    operator fun get(unit: Int, spike: Int) = spikeData[unit][spike]
+
+    /**
+     * @param unit - which unit
+     * @param spike - which spike
+     * @param value - which index inside [Spike.waveform]
+     * @return - The float corresponding to [unit], [spike] and [value]
+     */
+    operator fun get(unit: Int, spike: Int, value: Int) = spikeData[unit][spike][value]
+
 }
